e0d71e158b6ce8a6f66bf46004d4b2d19dad18dd
Pavan <pavanh933@gmail.com>
2024-02-06 12:04:53 +0530

    Upadating Dockefile for nodejs

--- Docker/Dockerfilecreation/app.js
+++ Docker/Dockerfilecreation/app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res,next)=>{
-    res.send('Welcome to express with docker');
+    res.send('<h1>Welcome to express with docker</h1>');
 })
 
-app.listen(3000,()=>{console.log("server started")});
+app.listen(80,()=>{console.log("server started")});
